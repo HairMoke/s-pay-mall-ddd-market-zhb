3cc092e4382855fd3c5a5b05a8e2e7e96d57731a
shall_zhao <shall_zhao@noreply.gitcode.com>
2025-04-08 11:15:37 +0800

    feat: 引入RabbitMQ分布式多端消费

---
+++ s-pay-mall-ddd-zhb-trigger/src/main/java/com/hb/trigger/listener/TeamSuccessTopicListener.java
@@ -0,0 +1,33 @@
+package com.hb.trigger.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 结算完成消息监听
+ */
+@Slf4j
+@Component
+public class TeamSuccessTopicListener {
+
+    /**
+     * 指定消费队列
+     */
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(value = "${spring.rabbitmq.config.consumer.topic_team_success.queue}"),
+                    exchange = @Exchange(value = "${spring.rabbitmq.config.consumer.topic_team_success.exchange}", type = ExchangeTypes.TOPIC),
+                    key = "${spring.rabbitmq.config.consumer.topic_team_success.routing_key}"
+            )
+    )
+    public void listener(String message) {
+        log.info("接收消息:{}", message);
+    }
+
+
+}
